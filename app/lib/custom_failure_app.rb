a63af91559634d53ae2f821509d703de29b756fe
Pablo Fernandez <pfer@me.com>
2019-10-28 10:57:02 +0100

    Hacky way of getting rid of hitting the user with a sign-up-first message
    Showing a "You need to sign in or sign up before continuing." as the first thing the user sees is not cool.
    
    Thinking about it, there's no use case in this app where that message would make sense, so let's just delete it when devise adds it.

--- app/lib/custom_failure_app.rb
+++ app/lib/custom_failure_app.rb
@@ -2,4 +2,12 @@ class CustomFailureApp < Devise::FailureApp
   def route(scope)
     :new_user_registration_url
   end
+
+  def redirect
+    super
+
+    if flash[:alert] == I18n.t('devise.failure.unauthenticated')
+      flash.delete :alert
+    end
+  end
 end
