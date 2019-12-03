da66989d8ef300184c9191b65317e57d94ee0eec
Pablo Fernandez <pfer@me.com>
2019-12-03 16:25:44 +0100

    Move raven credentials

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,7 +1,5 @@
 if Rails.env.production?
-  puts "Empty RAVEN_URL environmental variable" if ENV['RAVEN_URL'].empty?
-
   Raven.configure do |config|
-    config.dsn = ENV['RAVEN_URL']
+    config.dsn = Rails.application.credentials.config.raven
   end
 end
